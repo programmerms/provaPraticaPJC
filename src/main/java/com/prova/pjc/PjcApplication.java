7bd97abd777a2992dec893c423ff8a5091f71ed0
programmerms <programmerms@gmail.com>
2021-02-05 06:52:37 -0300

    Criaçao do projeto

---
+++ src/main/java/com/prova/pjc/PjcApplication.java
@@ -0,0 +1,13 @@
+package com.prova.pjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prova/pjc/PjcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prova.pjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
